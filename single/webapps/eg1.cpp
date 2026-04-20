d166073f2a3d4775ac570b92de7742a52bad93f4
Kanishk <itskanishk36@gmail.com>
2026-04-20 21:00:52 +0530

    updated

--- single/webapps/eg1.cpp
+++ single/webapps/eg1.cpp
@@ -33,10 +33,32 @@ void getCityView(Request &request,Response &response)
 {
 string cityCodeString=request.get("cityCode");
 int cityCode=atoi(cityCodeString.c_str());
-if(cityCode==1) request.forward((char *)"ujjain.html");
-else if(cityCode==2) request.forward((char *)"indore.html");
-else if(cityCode==3) request.forward((char *)"dewas.html");
-else request.forward((char *)"index.html");
+if(cityCode==1) request.forward("ujjain.html");
+else if(cityCode==2) request.forward("indore.html");
+else if(cityCode==3) request.forward("dewas.html");
+else
+{
+request.forward("errorPage");
+}
+}
+
+void createErrorPage(Request &request,Response &response)
+{
+response.write("<!DOCTYPE HTML>");
+response.write("<html lang='en'>");
+response.write("<head>");
+response.write("<meta charset='utf-8'>");
+response.write("<title>Error page</title>");
+response.write("</head>");
+response.write("<body>");
+response.write("<h1 style='color: green'>");
+response.write("Some Error Message");
+response.write("</h1>");
+response.write("<br>");
+response.write("<a href='index.html'>Home</a><br>");
+response.write("</body>");
+response.write("</html>");
+response.close();
 }
 int main()
 {
@@ -44,7 +66,7 @@ TMWebProjector server(7070);
 
 server.onRequest("/now",dispatchTime);
 server.onRequest("/getCity",getCityView);
-
+server.onRequest("/errorPage",createErrorPage);
 server.start();
 return 0;
 }
